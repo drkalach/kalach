91ae2d1a59add2f4afc4e97d155181782ecbe1fe
drkalach <kalach-2021@yandex.ru>
2021-06-30 01:40:39 +0000

    testing practice with an assert

---
+++ tests/capitalize.test.js
@@ -0,0 +1,9 @@
+import { strict as assert } from 'assert';
+import capitalize from '../src/capitalize.js';
+
+
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello');
+
+console.log('Все тесты пройдены!');
+  
